a50d1b1a7e17d8cdba4abe22a4840d8b9f15fa4a
bbronswijk <brambronswijk@gmail.com>
2024-09-14 14:48:38 +0200

    Have CTA overlap footer

--- src/app/[...filename]/CallToAction.tsx
+++ src/app/[...filename]/CallToAction.tsx
@@ -3,7 +3,10 @@ import { PageBlocksCall } from "../../../tina/__generated__/types";
 
 export function CallToAction({ title, text }: PageBlocksCall) {
   return (
-    <section className="rounded shadow-lg col-start-1 col-end-5 bg-gray-700 py-10 px-14 max-w-[900px] relative mx-auto">
+    <section
+      className="rounded shadow-lg col-start-1 col-end-5 bg-gray-700 py-10 px-14 max-w-[900px] relative mx-auto"
+      style={{ marginBottom: -120 }}
+    >
       <h2 className="font-medium text-3xl text-center">{title}</h2>
       <p className="mx-12 mt-5 mb-8 text-center">{text}</p>
       <form className="flex gap-8">
